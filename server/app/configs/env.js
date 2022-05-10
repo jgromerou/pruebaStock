9abef8f0b9ddab9765c3fdcef568a434e017eddd
jgromerou <jgromerouro@gmail.com>
2022-05-09 21:47:12 -0300

    Primera Subida

---
+++ server/app/configs/env.js
@@ -0,0 +1,21 @@
+// mis variables de ambiente
+const env = {
+  // para el express
+  listenOn: 'localhost',
+  puerto: 3000,
+  // para el postgres
+  database: 'stock',
+  usuario: 'root',
+  clave: 'divi1312',
+  host: 'localhost',
+  port: 3006,
+  dialect: 'mysql',
+  // http://docs.sequelizejs.com/manual/installation/usage.html
+  // pool de conexiones
+  max: 5,
+  min: 0,
+  idle: 30000,
+  acquire: 60000,
+};
+
+module.exports = env;
